0f6a56bf8a5436cb8dce5250108e1b90bf7b43c1
Dade Lamkins <dade@lamkins.org>
2019-05-28 04:55:38 -0400

    Minor code style cleanup.

--- Blish HUD/Modules/LoadingScreenHints/LoadingScreenHints.cs
+++ Blish HUD/Modules/LoadingScreenHints/LoadingScreenHints.cs
@@ -13,15 +13,11 @@ using Blish_HUD.Modules.LoadingScreenHints.Controls;
 
 namespace Blish_HUD.Modules.LoadingScreenHints
 {
-    public class LoadingScreenHints : Module
-    {
-        public override ModuleInfo GetModuleInfo()
-        {
-            Texture2D icon = null;
-
+    public class LoadingScreenHints : Module {
+        public override ModuleInfo GetModuleInfo() {
             return new ModuleInfo(
                 "Loading Screen Hints",
-                icon,
+                null,
                 "bh.general.loadingscreenhints",
                 "Shows tips, Tyrian knowledge, narrations and character quizzes during the loading screen.",
                 "Nekres.1038",
@@ -31,42 +27,31 @@ namespace Blish_HUD.Modules.LoadingScreenHints
 
         #region Settings
 
-        public override void DefineSettings(Settings settings)
-        {
+        public override void DefineSettings(Settings settings) {
             // Define settings
         }
 
         #endregion
+
         private LoadScreenPanel LoadScreenPanel;
 
-        public override void OnEnabled()
-        {
+        public override void OnEnabled() {
             base.OnEnabled();
             LoadScreenPanel = BuildLoadScreenPanel();
         }
-        private LoadScreenPanel BuildLoadScreenPanel()
-        {
-            var tipsPanel = new LoadScreenPanel()
-            {
+        private LoadScreenPanel BuildLoadScreenPanel() {
+            var tipsPanel = new LoadScreenPanel() {
                 Parent = GameService.Graphics.SpriteScreen
             };
             return tipsPanel;
         }
-        public override void OnDisabled()
-        {
-            sampleBuffer.Clear();
-            LoadScreenPanel.Dispose();
-        }
-        private long lastUpdate = 0;
+
         private Queue<double> sampleBuffer = new Queue<double>();
 
-        public override void Update(GameTime gameTime)
-        {
+        public override void Update(GameTime gameTime) {
             base.Update(gameTime);
 
-            // Unless we're in game running around, don't show.
-            if (!GameService.GameIntegration.IsInGame)
-            {
+            if (!GameService.GameIntegration.IsInGame) {
                 if (LoadScreenPanel.Fade != null) {
                     LoadScreenPanel.Fade.Cancel();
                     LoadScreenPanel.Fade = null;
@@ -79,8 +64,11 @@ namespace Blish_HUD.Modules.LoadingScreenHints
             }
 
             if (LoadScreenPanel.Fade == null) { LoadScreenPanel.FadeOut(); }
+        }
 
-            lastUpdate = GameService.Gw2Mumble.UiTick;
+        public override void OnDisabled() {
+            sampleBuffer.Clear();
+            LoadScreenPanel.Dispose();
         }
     }
 }
