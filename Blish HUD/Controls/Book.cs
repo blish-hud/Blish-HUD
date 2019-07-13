f063f760f587cd58e3809daee1dd575cb5d01f6c
Andreas G <andreas.gaertner93@gmail.com>
2019-07-13 19:47:24 +0200

    Book control.

--- Blish HUD/Controls/Book.cs
+++ Blish HUD/Controls/Book.cs
@@ -1,21 +1,21 @@
 ﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
+using MonoGame.Extended.BitmapFonts;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
 namespace Blish_HUD.Controls
 {
-    public class Book : WindowBase
+    public class Book : Control
     {
+        private static BitmapFont ContentFont = GameService.Content.GetFont(ContentService.FontFace.Menomonia, ContentService.FontSize.Size24, ContentService.FontStyle.Regular);
+        private static BitmapFont PageNumberFont = GameService.Content.GetFont(ContentService.FontFace.Menomonia, ContentService.FontSize.Size18, ContentService.FontStyle.Regular);
         private const int RIGHT_PADDING = 50;
         private const int TOP_PADDING = 100;
-
+        private const int FIX_WORDCLIPPING_WIDTH = 20;
         private readonly Texture2D BookSheetSprite;
-        private readonly Texture2D PanelBackgroundSprite;
-
         private Point sheetSize;
 
         private int currentPage = 0;
@@ -28,43 +28,31 @@ namespace Blish_HUD.Controls
             }
         }
 
-        public List<string> Contents { get; private set; }
+        public List<string> Contents;
 
         /// <summary>
         /// Creates a control similar to the Tyrian' sheet of paper or book control.
         /// </summary>
-        /// <param name="location">Location to draw the control.</param>
-        /// <param name="scale">The scale by which to scale the control.</param>
-        public Book() : base()
+        /// <param name="scale">Scale size to keep the sheet's aspect ratio.</param>
+        public Book(int scale = 1)
         {
-            Contents = new List<string>();
-
+            this.Size = new Point(625 * scale, 775 * scale);
             BookSheetSprite = BookSheetSprite ?? Content.GetTexture("1909316");
-            PanelBackgroundSprite = PanelBackgroundSprite ?? Content.GetTexture("1909321").Duplicate().GetRegion(0, 0, 660, 800);
-            Rectangle windowBackgroundBounds = new Rectangle(0, 0, 625, 775);
-
-            this.ContentRegion = windowBackgroundBounds;
-
-            // Construct background panel.
-            ConstructWindow(PanelBackgroundSprite, 
-                new Vector2(0, 0),
-                windowBackgroundBounds,
-                new Thickness(0, 0, 0, 35),
-                40,
-                true
-            );
-
             sheetSize = PointExtensions.ResizeKeepAspect(BookSheetSprite.Bounds.Size, this.Width - RIGHT_PADDING, this.Height - TOP_PADDING, true);
         }
-        public override void PaintBeforeChildren(SpriteBatch spriteBatch, Rectangle bounds)
+        protected override void Paint(SpriteBatch spriteBatch, Rectangle bounds)
         {
-            base.PaintBeforeChildren(spriteBatch, bounds);
-            Point center = new Point((this.ContentRegion.Width - sheetSize.X) / 2, (this.ContentRegion.Height - sheetSize.Y) / 2);
+            Point center = new Point((this.Size.X - sheetSize.X) / 2, (this.Size.Y - sheetSize.Y) / 2);
             spriteBatch.DrawOnCtrl(this, BookSheetSprite, new Rectangle(center, sheetSize), null, Color.White, 0f, Vector2.Zero, SpriteEffects.None);
 
-            if (Contents.Count > 0)
+            if (Contents != null && currentPage < Contents.Count)
             {
-                spriteBatch.DrawStringOnCtrl(this, Contents[currentPage], Content.DefaultFont14, BookSheetSprite.Bounds, Color.White, true, HorizontalAlignment.Left, VerticalAlignment.Top);
+                Rectangle contentArea = new Rectangle(new Point(center.X + 40, center.Y + 40), new Point(sheetSize.X - 80 - FIX_WORDCLIPPING_WIDTH, sheetSize.Y - 80));
+                spriteBatch.DrawStringOnCtrl(this, Contents[currentPage], ContentFont, contentArea, Color.Black, true, HorizontalAlignment.Left, VerticalAlignment.Top);
+                string pageNumber = (currentPage + 1).ToString();
+                Point pageNumberSize = (Point)PageNumberFont.MeasureString(pageNumber);
+                Point pageNumberCenter = new Point((this.Size.X - pageNumberSize.X) / 2, sheetSize.Y - (pageNumberSize.Y / 2));
+                spriteBatch.DrawStringOnCtrl(this, pageNumber, PageNumberFont, new Rectangle(pageNumberCenter, pageNumberSize), Color.Black, true, HorizontalAlignment.Left, VerticalAlignment.Top);
             }
         }
     }
