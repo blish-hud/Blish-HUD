f296e008f9ff87189d6f59cc95c4d490dac15fab
Greaka <mrgreaka@gmail.com>
2019-08-16 16:32:53 +0200

    fixed a small bug and added a common event

--- Blish HUD/GameServices/ArcDps/Common/CommonFields.cs
+++ Blish HUD/GameServices/ArcDps/Common/CommonFields.cs
@@ -5,44 +5,68 @@ namespace Blish_HUD.ArcDps.Common
     public class CommonFields
     {
         /// <summary>
-        /// Contains every player in the current group or squad.
-        /// Key: Character Name, Value: Account Name
+        ///     Delegate which will be invoked in <see cref="CommonFields.PlayerAdded" /> and
+        ///     <see cref="CommonFields.PlayerRemoved" />
+        /// </summary>
+        public delegate void PresentPlayersChange(string characterName, string accountName);
+
+        private readonly Dictionary<string, string> _playersInSquad = new Dictionary<string, string>();
+
+        private bool _enabled;
+
+        /// <summary>
+        ///     Contains every player in the current group or squad.
+        ///     Key: Character Name, Value: Account Name
         /// </summary>
         public IReadOnlyDictionary<string, string> PlayersInSquad => _playersInSquad;
 
-        private Dictionary<string, string> _playersInSquad = new Dictionary<string, string>();
+        /// <summary>
+        ///     Gets invoked whenever someone joins the squad or group.
+        /// </summary>
+        public event PresentPlayersChange PlayerAdded;
 
-        private bool _enabled = false;
+        /// <summary>
+        ///     Gets invoked whenever someone leaves the squad or group.
+        /// </summary>
+        public event PresentPlayersChange PlayerRemoved;
 
+        /// <summary>
+        ///     Activates the <see cref="CommonFields"/> service.
+        /// </summary>
         public void Activate()
         {
             if (_enabled)
                 return;
 
+            _enabled = true;
             GameService.ArcDps.RawCombatEvent += CombatHandler;
         }
 
         private void CombatHandler(object sender, RawCombatEventArgs args)
         {
             if (args.CombatEvent.Ev != null) return;
-            
+
             /* notify tracking change */
             if (args.CombatEvent.Src.Elite != 0) return;
-            
+
             /* add */
             if (args.CombatEvent.Src.Profession != 0)
             {
                 if (!_playersInSquad.ContainsKey(args.CombatEvent.Src.Name))
                 {
                     _playersInSquad.Add(args.CombatEvent.Src.Name, args.CombatEvent.Dst.Name);
+                    PlayerAdded?.Invoke(args.CombatEvent.Src.Name, args.CombatEvent.Dst.Name);
                 }
             }
-
             /* remove */
             else
             {
                 if (_playersInSquad.ContainsKey(args.CombatEvent.Src.Name))
+                {
+                    var accountName = _playersInSquad[args.CombatEvent.Src.Name];
                     _playersInSquad.Remove(args.CombatEvent.Src.Name);
+                    PlayerRemoved?.Invoke(args.CombatEvent.Src.Name, accountName);
+                }
             }
         }
     }
