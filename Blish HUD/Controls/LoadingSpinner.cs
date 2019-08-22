23389fe1edc5e585e24c7153420c5af9b10c15e6
Dade Lamkins <dade@lamkins.org>
2019-08-22 01:14:00 -0400

    Added new "LoadingSpinnerUtil" and switched the LoadingSpinner to use the util.

--- Blish HUD/Controls/LoadingSpinner.cs
+++ Blish HUD/Controls/LoadingSpinner.cs
@@ -4,30 +4,14 @@ using Microsoft.Xna.Framework.Graphics;
 namespace Blish_HUD.Controls {
     public class LoadingSpinner : Control {
 
-        private const string DRAWATLAS = "spinner-atlas";
         private const int DRAWLENGTH = 64;
-
-        private Rectangle _activeAtlasRegion = new Rectangle(0, 416, 256, 32);
-
-        public int Rotation { get; set; } = 0;
 
         public LoadingSpinner() {
             this.Size = new Point(DRAWLENGTH, DRAWLENGTH);
-
-            Animation.Tweener.Tween(this, new { Rotation = 64 }, 3).Repeat().Round();
-        }
-
-        public override void DoUpdate(GameTime gameTime) {
-            _activeAtlasRegion = new Rectangle(DRAWLENGTH * this.Rotation, 0, DRAWLENGTH, DRAWLENGTH);
-        }
-
-        protected override CaptureType CapturesInput() {
-            return CaptureType.Mouse;
         }
 
         protected override void Paint(SpriteBatch spriteBatch, Rectangle bounds) {
-            // TODO: Add this texture in with the rest of the UI elements in the ControlUI atlas
-            spriteBatch.DrawOnCtrl(this, Content.GetTexture(DRAWATLAS), bounds, _activeAtlasRegion);
+            LoadingSpinnerUtil.DrawLoadingSpinner(this, spriteBatch, bounds);
         }
 
     }
---
+++ Blish HUD/_Utils/LoadingSpinnerUtil.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Blish_HUD.Controls;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace Blish_HUD {
+    public static class LoadingSpinnerUtil {
+
+        #region Load Static
+
+        private static readonly Texture2D _loadingSpinnerTexture;
+
+        static LoadingSpinnerUtil() {
+            _loadingSpinnerTexture = GameService.Content.GetTexture("spinner-atlas");
+        }
+
+        #endregion
+
+        /// <summary>
+        /// Draws an animated loading spinner at the provided <param name="bounds">bounds</param>.
+        /// </summary>
+        /// <param name="control">The control the loading spinner will be drawn on.</param>
+        /// <param name="spriteBatch">The active spritebatch.</param>
+        /// <param name="bounds">The location to draw the loading spinner.</param>
+        public static void DrawLoadingSpinner(Control control, SpriteBatch spriteBatch, Rectangle bounds) {
+            spriteBatch.DrawOnCtrl(control,
+                                   _loadingSpinnerTexture,
+                                   bounds,
+                                   new Rectangle(((int)(GameService.Overlay.CurrentGameTime.TotalGameTime.TotalSeconds * (64f / 3f))) % 64 * 64, 0, 64, 64));
+        }
+
+    }
+}
