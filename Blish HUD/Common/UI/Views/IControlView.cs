cb33c07a25748259b6654538a1548b71ebc2e026
Dade Lamkins <dade@lamkins.org>
2019-11-08 01:26:46 -0500

    More class splitting for codefactor.

---
+++ Blish HUD/Common/UI/Views/IControlView.cs
@@ -0,0 +1,11 @@
+﻿using Blish_HUD.Controls;
+
+namespace Blish_HUD.Common.UI.Views {
+
+    public interface IControlView<out TControl> where TControl : Control {
+
+        TControl Control { get; }
+
+    }
+
+}
