309ea7df35b17111be5c07b03fda70217949e9cd
Dade Lamkins <dade@lamkins.org>
2019-12-03 03:34:08 -0500

    Added Common.KeyBindings to contain the built-in GW2 bindings for modules and other services to attach to.

---
+++ Blish HUD/Common/Gw2/KeyBindings.cs
@@ -0,0 +1,18 @@
+﻿using Blish_HUD.Input;
+using Microsoft.Xna.Framework.Input;
+
+namespace Blish_HUD.Common.Gw2 {
+
+    /// <summary>
+    /// A collection of the built-in GW2 keybindings.
+    /// </summary>
+    public static class KeyBindings {
+
+        /// <summary>
+        /// General context-sensitive interact prompt.
+        /// Used for interacting with the environment, including Talk, Loot, Revive, etc.
+        /// </summary>
+        public static readonly KeyBinding Interact = new KeyBinding(Keys.F) { Enabled = true };
+
+    }
+}
