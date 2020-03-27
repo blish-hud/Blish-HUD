8504b360eb0f20666d3cf21b44c7d4d53c707dd8
Denrage <denrage@gmail.com>
2020-03-27 20:06:03 +0100

    Fixed brace styling

--- Blish HUD/_Extensions/ColorExtensions.cs
+++ Blish HUD/_Extensions/ColorExtensions.cs
@@ -5,13 +5,14 @@ using System.Text;
 using System.Threading.Tasks;
 using Microsoft.Xna.Framework;
 
-namespace Blish_HUD._Extensions
-{
-    public static class ColorExtensions
-    {
-        public static Microsoft.Xna.Framework.Color ToXnaColor(this Gw2Sharp.WebApi.V2.Models.ColorMaterial color)
-        {
-            return  new Color(color.Rgb[0], color.Rgb[1], color.Rgb[2]);
+namespace Blish_HUD._Extensions {
+
+    public static class ColorExtensions {
+
+        public static Microsoft.Xna.Framework.Color ToXnaColor(this Gw2Sharp.WebApi.V2.Models.ColorMaterial color) {
+            return new Color(color.Rgb[0], color.Rgb[1], color.Rgb[2]);
         }
+
     }
+
 }
