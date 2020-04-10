0ef9f1edcd020c72da9f9a5daf65e79237c8fde8
Dade Lamkins <dade@lamkins.org>
2020-04-09 22:07:11 -0400

    Updated for latest Gw2Sharp changes and added localized strings under Strings.GameServices.Contexts.Festival.

--- Blish HUD/GameServices/Contexts/FestivalContext.cs
+++ Blish HUD/GameServices/Contexts/FestivalContext.cs
@@ -2,11 +2,8 @@
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Linq;
-using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
-using Blish_HUD.Properties;
-using Gw2Sharp.WebApi.Http;
 
 namespace Blish_HUD.Contexts {
 
@@ -15,7 +12,7 @@ namespace Blish_HUD.Contexts {
     /// </summary>
     public class FestivalContext : Context {
 
-        public static readonly Logger Logger = Logger.GetLogger(typeof(FestivalContext));
+        private static readonly Logger Logger = Logger.GetLogger<FestivalContext>();
 
         /// <summary>
         /// A Guild Wars 2 festival identified by the <see cref="FestivalContext"/>.
@@ -60,32 +57,32 @@ namespace Blish_HUD.Contexts {
             /// <summary>
             /// Guild Wars 2 Halloween Festival.
             /// </summary>
-            public static readonly Festival Halloween = new Festival("halloween", Strings.Context_FestivalContext_Halloween);
+            public static readonly Festival Halloween = new Festival("halloween", Strings.GameServices.Contexts.Festivals.Festival_Halloween);
 
             /// <summary>
             /// Guild Wars 2 Wintersday Festival.
             /// </summary>
-            public static readonly Festival Wintersday = new Festival("wintersday", Strings.Context_FestivalContext_Wintersday);
+            public static readonly Festival Wintersday = new Festival("wintersday", Strings.GameServices.Contexts.Festivals.Festival_Wintersday);
 
             /// <summary>
             /// Guild Wars 2 Super Adventure Festival.
             /// </summary>
-            public static readonly Festival SuperAdventureFestival = new Festival("superadventurefestival", Strings.Context_FestivalContext_SuperAdventureFestival);
+            public static readonly Festival SuperAdventureFestival = new Festival("superadventurefestival", Strings.GameServices.Contexts.Festivals.Festival_SuperAdventureFestival);
 
             /// <summary>
             /// Guild Wars 2 Lunar New Year Festival.
             /// </summary>
-            public static readonly Festival LunarNewYear = new Festival("lunarnewyear", Strings.Context_FestivalContext_LunarNewYear);
+            public static readonly Festival LunarNewYear = new Festival("lunarnewyear", Strings.GameServices.Contexts.Festivals.Festival_LunarNewYear);
 
             /// <summary>
             /// Guild Wars 2 Festival of the Four Winds.
             /// </summary>
-            public static readonly Festival FestivalOfTheFourWinds = new Festival("festivalofthefourwinds", Strings.Context_FestivalContext_FestivalOfTheFourWinds);
+            public static readonly Festival FestivalOfTheFourWinds = new Festival("festivalofthefourwinds", Strings.GameServices.Contexts.Festivals.Festival_FestivalOfTheFourWinds);
 
             /// <summary>
             /// Guild Wars 2 Dragon Bash Festival.
             /// </summary>
-            public static readonly Festival DragonBash = new Festival("dragonbash", Strings.Context_FestivalContext_DragonBash);
+            public static readonly Festival DragonBash = new Festival("dragonbash", Strings.GameServices.Contexts.Festivals.Festival_DragonBash);
 
         }
 
@@ -142,9 +139,9 @@ namespace Blish_HUD.Contexts {
             _fault = null;
 
             try {
-                var dailyAchievementCategories = await GameService.Gw2Api.SharedClient.V2.Achievements.Groups.GetAsync(Guid.Parse(DAILY_GROUP_ID), cancellationToken);
+                var dailyAchievementCategories = await GameService.Gw2WebApi.AnonymousConnection.Client.V2.Achievements.Groups.GetAsync(Guid.Parse(DAILY_GROUP_ID), cancellationToken);
 
-                return dailyAchievementCategories.Categories.Where((category) => _knownFestivalCategories.ContainsKey(category)).Select((category) => _knownFestivalCategories[category]);
+                return dailyAchievementCategories.Categories.Where(category => _knownFestivalCategories.ContainsKey(category)).Select(category => _knownFestivalCategories[category]);
             } catch (Exception e) {
                 _fault = $"Failed to query Guild Wars 2 API: {e.Message}";
 
