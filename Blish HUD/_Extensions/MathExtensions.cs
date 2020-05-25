779e597522ffa48bca3bbb72cfde6fd0ebe9a22e
Andreas G <andreas.gaertner93@gmail.com>
2020-05-25 10:22:55 +0200

    Added MathExtensions

---
+++ Blish HUD/_Extensions/MathExtensions.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Blish_HUD {
+    public static class MathExtensions {
+        /// <summary>
+        /// Restricts a value to be between a minimum and a maximum.
+        /// </summary>
+        public static T Clamp<T>(this T val, T min, T max) where T : IComparable<T> {
+            return val.CompareTo(min) < 0 ? min : val.CompareTo(max) > 0 ? max : val;
+        }
+    }
+}
