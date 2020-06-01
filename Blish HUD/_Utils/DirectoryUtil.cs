f93ea3172b9d7925c4c0a2151ea20fce06866402
Andreas G <andreas.gaertner93@gmail.com>
2020-06-01 19:28:45 +0200

    matching comments

--- Blish HUD/_Utils/DirectoryUtil.cs
+++ Blish HUD/_Utils/DirectoryUtil.cs
@@ -20,17 +20,17 @@ namespace Blish_HUD {
 
         /// <summary>
         /// The current root application save path used for saving settings, letting modules save data, etc.
-        /// By default it is found in "Documents\GUILD WARS 2\addons\blishhud."
+        /// By default it is found in "Documents\Guild Wars 2\addons\blishhud."
         /// </summary>
         public static string BasePath { get; }
         /// <summary>
         /// The path used by the game client for saving screenshots made in the game (usually using the print screen key).
-        /// By default it is found in "Documents\GUILD WARS 2\Screens."
+        /// By default it is found in "Documents\Guild Wars 2\Screens."
         /// </summary>
         public static string ScreensPath { get; }
         /// <summary>
         /// The path used by the game client for loading custom music playlists in a context-sensitive manner during gameplay.
-        /// By default it is found in "Documents\GUILD WARS 2\music."
+        /// By default it is found in "Documents\Guild Wars 2\Music."
         /// </summary>
         public static string MusicPath { get; }
 
@@ -38,21 +38,21 @@ namespace Blish_HUD {
             // Prepare user documents directory
             BasePath = ApplicationSettings.Instance.UserSettingsPath
                     ?? Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments,
-                                                               Environment.SpecialFolderOption.DoNotVerify),
-                                     ADDON_DIR);
+                                                               Environment.SpecialFolderOption.DoNotVerify), 
+                                    ADDON_DIR);
 
             Directory.CreateDirectory(BasePath);
 
             ScreensPath = ApplicationSettings.Instance.UserSettingsPath
                        ?? Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments,
-                                                                 Environment.SpecialFolderOption.DoNotVerify),
+                                                                 Environment.SpecialFolderOption.DoNotVerify), 
                                        SCREENS_DIR);
 
             Directory.CreateDirectory(ScreensPath);
 
             MusicPath = ApplicationSettings.Instance.UserSettingsPath
                      ?? Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments,
-                                                               Environment.SpecialFolderOption.DoNotVerify),
+                                                               Environment.SpecialFolderOption.DoNotVerify), 
                                      MUSIC_DIR);
 
             Directory.CreateDirectory(MusicPath);
