309af67318815a886ce42f53a11eec84ae377c9b
Andreas G <andreas.gaertner93@gmail.com>
2020-06-02 00:32:01 +0200

    removed logger, cleanup usings

--- Blish HUD/_Utils/ClipboardUtil.cs
+++ Blish HUD/_Utils/ClipboardUtil.cs
@@ -1,9 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Runtime.InteropServices;
-using System.Threading;
-using System.Windows.Forms;
 using AsyncWindowsClipboard;
 
 namespace Blish_HUD {
@@ -12,8 +7,6 @@ namespace Blish_HUD {
     /// used to make async calls to the Windows clipboard.
     /// </summary>
     public static class ClipboardUtil {
-
-        private static readonly Logger Logger = Logger.GetLogger(typeof(ClipboardUtil));
 
         private static readonly WindowsClipboardService _clipboardService;
 
