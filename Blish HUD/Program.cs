072c83ba80fa9c07a43a1bb4364472009de45eac
Dade Lamkins <dade@lamkins.org>
2020-06-10 20:52:21 -0400

    Enabled dynamic mutex name if mumble mapping name is specified to allow for multiboxing.

--- Blish HUD/Program.cs
+++ Blish HUD/Program.cs
@@ -14,6 +14,8 @@ namespace Blish_HUD {
 
         private static Logger Logger;
 
+        private const string APP_GUID = "{5802208e-71ca-4745-ab1b-d851bc17a460}";
+
         public static SemVer.Version OverlayVersion { get; } = new SemVer.Version(typeof(BlishHud).Assembly.GetCustomAttribute<AssemblyInformationalVersionAttribute>().InformationalVersion, true);
 
         private static void EnableLogging() {
@@ -29,7 +31,7 @@ namespace Blish_HUD {
             }
         }
 
-        private static readonly Mutex SingleInstanceMutex = new Mutex(true, "{5802208e-71ca-4745-ab1b-d851bc17a460}");
+        private static Mutex _singleInstanceMutex;
 
         /// <summary>
         /// The main entry point for the application.
@@ -44,19 +46,22 @@ namespace Blish_HUD {
 
             Logger.Debug("Launched from {launchDirectory} with args {launchOptions}.", Directory.GetCurrentDirectory(), string.Join(" ", args));
 
-            if (IsMoreThanOneInstance()) {
+            // Single instance handling
+            _singleInstanceMutex = new Mutex(true, ApplicationSettings.Instance.MumbleMapName != null
+                                                       ? $"{APP_GUID}:{ApplicationSettings.Instance.MumbleMapName}"
+                                                       : $"{APP_GUID}");
+
+            if (!_singleInstanceMutex.WaitOne(TimeSpan.Zero, true)) {
                 Logger.Warn("Blish HUD is already running!");
                 return;
             }
 
-            // Needed by textboxes to enable CTRL + A selection
-            Application.EnableVisualStyles();
+            using (var game = new BlishHud()) {
+                game.Run();
+            }
 
-            using (var game = new BlishHud()) game.Run();
-            SingleInstanceMutex.ReleaseMutex();
+            _singleInstanceMutex.ReleaseMutex();
         }
-
-        private static bool IsMoreThanOneInstance() => !SingleInstanceMutex.WaitOne(TimeSpan.Zero, true);
 
     }
 
