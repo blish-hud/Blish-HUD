20318da26d20237ec69cf1390d3e251ff086adbf
greaka <git@greaka.de>
2020-08-05 21:32:13 +0200

    added braces and ran code cleanup

--- Blish HUD/GameServices/Input/Mouse/MouseHandler.cs
+++ Blish HUD/GameServices/Input/Mouse/MouseHandler.cs
@@ -9,51 +9,26 @@ namespace Blish_HUD.Input {
 
         private static readonly Logger Logger = Logger.GetLogger<MouseHandler>();
 
-        #region Events
-
-        public event EventHandler<MouseEventArgs> MouseMoved;
-        public event EventHandler<MouseEventArgs> LeftMouseButtonPressed;
-        public event EventHandler<MouseEventArgs> LeftMouseButtonReleased;
-        public event EventHandler<MouseEventArgs> RightMouseButtonPressed;
-        public event EventHandler<MouseEventArgs> RightMouseButtonReleased;
-        public event EventHandler<MouseEventArgs> MouseWheelScrolled;
-
-        #endregion
-
         /// <summary>
-        /// The current position of the mouse relative to the application.
+        ///     The current position of the mouse relative to the application.
         /// </summary>
         public Point Position => this.State.Position;
 
         public Point PositionRaw { get; private set; }
 
         /// <summary>
-        /// The current state of the mouse.
+        ///     The current state of the mouse.
         /// </summary>
         public MouseState State { get; private set; }
 
-        private MouseEventArgs _mouseEvent;
-
-        private bool _cameraDragging;
-
-        private bool _hudFocused;
-
         /// <summary>
-        /// Indicates if the <see cref="ActiveControl"/> has <see cref="Control.Captures"/>
-        /// set to <see cref="CaptureType.ForceNone"/>.
-        /// </summary>
-        private bool _hookOverride;
-
-        private Control _activeControl;
-
-        /// <summary>
-        /// The <see cref="Control"/> that the mouse last moved over.
+        ///     The <see cref="Control" /> that the mouse last moved over.
         /// </summary>
         public Control ActiveControl {
             get => _activeControl;
             private set {
                 _hudFocused   = value != null;
-                _hookOverride = (value != null) && value.Captures.HasFlag(CaptureType.ForceNone);
+                _hookOverride = value != null && value.Captures.HasFlag(CaptureType.ForceNone);
 
                 _activeControl = value;
 
@@ -61,84 +36,36 @@ namespace Blish_HUD.Input {
             }
         }
 
+        private Control _activeControl;
+
+        private bool _cameraDragging;
+
+        /// <summary>
+        ///     Indicates if the <see cref="ActiveControl" /> has <see cref="Control.Captures" />
+        ///     set to <see cref="CaptureType.ForceNone" />.
+        /// </summary>
+        private bool _hookOverride;
+
+        private bool _hudFocused;
+
+        private MouseEventArgs _mouseEvent;
+
         internal MouseHandler() { }
-
-        public void Update() {
-            if (!GameService.GameIntegration.Gw2IsRunning || !GameService.GameIntegration.Gw2HasFocus) {
-                _hudFocused = false;
-                return;
-            }
-
-            if (_cameraDragging) return;
-
-            MouseState prevMouseState = this.State;
-
-            MouseState rawMouseState = Mouse.GetState();
-
-            this.State = new MouseState(
-                                        (int) (rawMouseState.X / GameService.Graphics.UIScaleMultiplier),
-                                        (int) (rawMouseState.Y / GameService.Graphics.UIScaleMultiplier),
-                                        _mouseEvent?.WheelDelta ?? 0,
-                                        rawMouseState.LeftButton,
-                                        rawMouseState.MiddleButton,
-                                        rawMouseState.RightButton,
-                                        rawMouseState.XButton1,
-                                        rawMouseState.XButton2
-                                       );
-
-            // Handle mouse moved
-            if (prevMouseState.Position != this.State.Position) {
-                if (_hookOverride) this.ActiveControl = this.ActiveControl.MouseOver ? this.ActiveControl : null;
-
-                this.ActiveControl = GameService.Graphics.SpriteScreen.TriggerMouseInput(MouseEventType.MouseMoved, this.State);
-                this.MouseMoved?.Invoke(this, new MouseEventArgs(MouseEventType.MouseMoved));
-            }
-
-            // Handle mouse events blocked by the mouse hook
-            if (_mouseEvent != null) {
-                if (HandleHookedMouseEvent(_mouseEvent)) GameService.Graphics.SpriteScreen.TriggerMouseInput(_mouseEvent.EventType, this.State);
-
-                _mouseEvent = null;
-            }
-
-            // Handle mouse left pressed/released
-            if (prevMouseState.LeftButton != this.State.LeftButton) {
-                if (this.State.LeftButton == ButtonState.Pressed)
-                    this.LeftMouseButtonPressed?.Invoke(this, new MouseEventArgs(MouseEventType.LeftMouseButtonPressed));
-                else if (this.State.LeftButton == ButtonState.Released) this.LeftMouseButtonReleased?.Invoke(this, new MouseEventArgs(MouseEventType.LeftMouseButtonReleased));
-            }
-
-            // Handle mouse right pressed/released
-            if (prevMouseState.RightButton != this.State.RightButton) {
-                if (this.State.RightButton == ButtonState.Pressed)
-                    this.RightMouseButtonPressed?.Invoke(this, new MouseEventArgs(MouseEventType.RightMouseButtonPressed));
-                else if (this.State.RightButton == ButtonState.Released) this.RightMouseButtonReleased?.Invoke(this, new MouseEventArgs(MouseEventType.RightMouseButtonReleased));
-            }
-
-            // Handle mouse scroll
-            if (this.State.ScrollWheelValue != 0) this.MouseWheelScrolled?.Invoke(this, new MouseEventArgs(MouseEventType.MouseWheelScrolled));
-        }
-
-        public void OnEnable() {
-            /* NOOP */
-        }
-
-        public void OnDisable() {
-            /* NOOP */
-        }
 
         public bool HandleInput(MouseEventArgs mouseEventArgs) {
             if (mouseEventArgs.EventType == MouseEventType.MouseMoved) {
-                PositionRaw = new Point(mouseEventArgs.PointX, mouseEventArgs.PointY);
+                this.PositionRaw = new Point(mouseEventArgs.PointX, mouseEventArgs.PointY);
                 return false;
             }
 
-            if (_cameraDragging && mouseEventArgs.EventType == MouseEventType.RightMouseButtonReleased)
+            if (_cameraDragging && mouseEventArgs.EventType == MouseEventType.RightMouseButtonReleased) {
                 _cameraDragging = false;
-            else if (_hudFocused && !_hookOverride) {
+            } else if (_hudFocused && !_hookOverride) {
                 _mouseEvent = mouseEventArgs;
                 return mouseEventArgs.EventType != MouseEventType.LeftMouseButtonReleased;
-            } else if (mouseEventArgs.EventType == MouseEventType.RightMouseButtonPressed) _cameraDragging = true;
+            } else if (mouseEventArgs.EventType == MouseEventType.RightMouseButtonPressed) {
+                _cameraDragging = true;
+            }
 
             return false;
         }
@@ -167,6 +94,99 @@ namespace Blish_HUD.Input {
 
             return true;
         }
+
+        public void Update() {
+            if (!GameService.GameIntegration.Gw2IsRunning || !GameService.GameIntegration.Gw2HasFocus) {
+                _hudFocused = false;
+                return;
+            }
+
+            if (_cameraDragging) {
+                return;
+            }
+
+            var prevMouseState = this.State;
+
+            var rawMouseState = Mouse.GetState();
+
+            this.State = new MouseState(
+                                        (int) (rawMouseState.X / GameService.Graphics.UIScaleMultiplier),
+                                        (int) (rawMouseState.Y / GameService.Graphics.UIScaleMultiplier),
+                                        _mouseEvent?.WheelDelta ?? 0,
+                                        rawMouseState.LeftButton,
+                                        rawMouseState.MiddleButton,
+                                        rawMouseState.RightButton,
+                                        rawMouseState.XButton1,
+                                        rawMouseState.XButton2
+                                       );
+
+            // Handle mouse moved
+            if (prevMouseState.Position != this.State.Position) {
+                if (_hookOverride) {
+                    this.ActiveControl = this.ActiveControl.MouseOver ? this.ActiveControl : null;
+                }
+
+                this.ActiveControl = GameService.Graphics.SpriteScreen.TriggerMouseInput(MouseEventType.MouseMoved, this.State);
+                this.MouseMoved?.Invoke(this, new MouseEventArgs(MouseEventType.MouseMoved));
+            }
+
+            // Handle mouse events blocked by the mouse hook
+            if (_mouseEvent != null) {
+                if (HandleHookedMouseEvent(_mouseEvent)) {
+                    GameService.Graphics.SpriteScreen.TriggerMouseInput(_mouseEvent.EventType, this.State);
+                }
+
+                _mouseEvent = null;
+            }
+
+            // Handle mouse left pressed/released
+            if (prevMouseState.LeftButton != this.State.LeftButton) {
+                switch (this.State.LeftButton) {
+                    case ButtonState.Pressed:
+                        this.LeftMouseButtonPressed?.Invoke(this, new MouseEventArgs(MouseEventType.LeftMouseButtonPressed));
+                        break;
+                    case ButtonState.Released:
+                        this.LeftMouseButtonReleased?.Invoke(this, new MouseEventArgs(MouseEventType.LeftMouseButtonReleased));
+                        break;
+                }
+            }
+
+            // Handle mouse right pressed/released
+            if (prevMouseState.RightButton != this.State.RightButton) {
+                switch (this.State.RightButton) {
+                    case ButtonState.Pressed:
+                        this.RightMouseButtonPressed?.Invoke(this, new MouseEventArgs(MouseEventType.RightMouseButtonPressed));
+                        break;
+                    case ButtonState.Released:
+                        this.RightMouseButtonReleased?.Invoke(this, new MouseEventArgs(MouseEventType.RightMouseButtonReleased));
+                        break;
+                }
+            }
+
+            // Handle mouse scroll
+            if (this.State.ScrollWheelValue != 0) {
+                this.MouseWheelScrolled?.Invoke(this, new MouseEventArgs(MouseEventType.MouseWheelScrolled));
+            }
+        }
+
+        public void OnEnable() {
+            /* NOOP */
+        }
+
+        public void OnDisable() {
+            /* NOOP */
+        }
+
+        #region Events
+
+        public event EventHandler<MouseEventArgs> MouseMoved;
+        public event EventHandler<MouseEventArgs> LeftMouseButtonPressed;
+        public event EventHandler<MouseEventArgs> LeftMouseButtonReleased;
+        public event EventHandler<MouseEventArgs> RightMouseButtonPressed;
+        public event EventHandler<MouseEventArgs> RightMouseButtonReleased;
+        public event EventHandler<MouseEventArgs> MouseWheelScrolled;
+
+        #endregion
 
     }
 
