2adfea3d6e9de682e664fed0b343896b657d24df
greaka <git@greaka.de>
2020-08-16 08:03:43 +0200

    trails are now preprocessed before creation

--- Blish HUD/_Extensions/Vector3Extensions.cs
+++ Blish HUD/_Extensions/Vector3Extensions.cs
@@ -1,6 +1,8 @@
 ﻿using System;
+using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
 using Gw2Sharp.Models;
 using Microsoft.Xna.Framework;
 
@@ -161,6 +163,57 @@ namespace Blish_HUD {
             }
             hermitePoints.Add(points.Last());
             return hermitePoints;
+        }
+        
+        public static List<Vector3> DouglasPeucker(this List<Vector3> vectors, float error = 0.2f) {
+            if (vectors.Count < 3) {
+                return new List<Vector3>(vectors);
+            }
+
+            // indices to points to keep
+            var keep = new ConcurrentBag<int > {
+                0,
+                vectors.Count - 1
+            };
+            Recursive(vectors, error, 0, vectors.Count - 1, keep);
+            List<int> keepList = keep.ToList();
+            keepList.Sort();
+            return keepList.Select(i => vectors[i]).ToList();
+        }
+
+        private static void Recursive(IReadOnlyList<Vector3> vectors, float error, int first, int last, ConcurrentBag<int> keep) {
+            if (last - first + 1 < 3) {
+                return;
+            }
+
+            var vFirst = vectors[first];
+            var vLast  = vectors[last];
+
+            var lastToFirst = vLast - vFirst;
+            float length           = lastToFirst.Length();
+            float maxDist = error;
+            int split   = 0;
+
+            for (int i = first + 1; i < last; i++) {
+                var v = vectors[i];
+                // distance to line vFirst -> vLast
+                float dist = Vector3.Cross(vFirst - v, lastToFirst).Length() / length;
+
+                if (dist < maxDist) continue;
+
+                maxDist = dist;
+                split   = i;
+            }
+
+            if (split == 0) return;
+
+            keep.Add(split);
+            var tasks = new Task[2];
+            tasks[0] = Task.Run(() => Recursive(vectors, error, first, split, keep));
+            tasks[1] = Task.Run(() => Recursive(vectors, error, split, last,  keep));
+            foreach (var task in tasks) {
+                task.Wait();
+            }
         }
     }
 }
