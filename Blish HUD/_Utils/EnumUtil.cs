361382e88ba18850d236251495b60a8eeb3c9862
Dade Lamkins <dade@lamkins.org>
2020-10-17 19:38:37 -0400

    Added EnumComplianceRequisite.  Temporarily removed Chinese from overlay language settings.

---
+++ Blish HUD/_Utils/EnumUtil.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Blish_HUD {
+    public static class EnumUtil {
+
+        private static readonly Dictionary<Type, Array> _cachedEnumValues = new Dictionary<Type, Array>();
+
+        /// <summary>
+        /// Returns the individual values in an enum as an array.
+        /// The results are cached so future calls do not repeat calls to <see cref="Enum.GetValues"/>.
+        /// </summary>
+        public static T[] GetCachedValues<T>() where T : Enum {
+            if (!_cachedEnumValues.ContainsKey(typeof(T))) {
+                _cachedEnumValues.Add(typeof(T), Enum.GetValues(typeof(T)));
+            }
+
+            return _cachedEnumValues[typeof(T)].Cast<T>().ToArray();
+        }
+
+    }
+}
