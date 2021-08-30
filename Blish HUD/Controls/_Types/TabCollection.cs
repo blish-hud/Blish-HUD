d679e242632ab2b1673b85213ad42e1933cb2906
Dade Lamkins <dade@lamkins.org>
2021-08-30 03:13:45 -0400

    Added docs to TabCollection.

--- Blish HUD/Controls/_Types/TabCollection.cs
+++ Blish HUD/Controls/_Types/TabCollection.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 
 namespace Blish_HUD.Controls {
+    /// <summary>
+    /// A collection of tabs, sorted from largest <see cref="Tab.OrderPriority"/> to the smallest.
+    /// </summary>
     public class TabCollection : ICollection<Tab> {
 
         private List<Tab> _tabs = new List<Tab>();
@@ -47,10 +50,16 @@ namespace Blish_HUD.Controls {
             return _tabs.Remove(item);
         }
 
+        /// <summary>
+        /// Returns the index of the provided <see cref="Tab"/>.  If the <see cref="Tab"/> is not within the collection, -1 is returned.
+        /// </summary>
         public int IndexOf(Tab tab) {
             return _tabs.IndexOf(tab);
         }
 
+        /// <summary>
+        /// Returns the <see cref="Tab"/> at the provided index based on <see cref="Tab.OrderPriority"/>.
+        /// </summary>
         public Tab FromIndex(int tabIndex) {
             if (tabIndex >= 0 && tabIndex < _tabs.Count) {
                 return _tabs[tabIndex];
