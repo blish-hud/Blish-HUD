5f5b3b5bd7a6cfbb8ef4909921b6fdde4b10a2c8
Dade Lamkins <dade@lamkins.org>
2021-10-20 20:37:58 -0400

    https://sonarcloud.io/project/issues?branch=dev&id=blish-hud_Blish-HUD&issues=AXrHsx-wPc0PJb7Sd6KR&open=AXrHsx-wPc0PJb7Sd6KR

--- Blish HUD/Controls/BackButton.cs
+++ Blish HUD/Controls/BackButton.cs
@@ -13,11 +13,7 @@ namespace Blish_HUD.Controls {
 
         #region Load Static
 
-        private static readonly Texture2D _textureBackButton;
-
-        static BackButton() {
-            _textureBackButton = Content.GetTexture("784268");
-        }
+        private static readonly Texture2D _textureBackButton = Content.GetTexture("784268");
 
         #endregion
 
