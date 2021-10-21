ecbb058a1553fa515db12425ae19ffe6f0951a4d
Dade Lamkins <dade@lamkins.org>
2021-10-20 20:55:47 -0400

    Cleaned some using statements.

--- Blish HUD/Controls/ScreenNotification.cs
+++ Blish HUD/Controls/ScreenNotification.cs
@@ -1,7 +1,5 @@
 ﻿using System.Collections.Generic;
 using System.Linq;
-using Blish_HUD;
-using Glide;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using MonoGame.Extended.BitmapFonts;
