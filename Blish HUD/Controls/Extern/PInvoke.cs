0d46960562002d652e8bfff0ebe22eedae18c27e
Dade Lamkins <dade@lamkins.org>
2022-02-23 22:13:33 -0500

    Updated our calls to GetCursorInfo to expose if the cursor is visible or not to, for example, detect if action cam is enabled.  Further, we avoid passing input updates to our UI pipeline if the cursor isn't visible (but other events will still fire).

--- Blish HUD/Controls/Extern/PInvoke.cs
+++ Blish HUD/Controls/Extern/PInvoke.cs
@@ -8,38 +8,16 @@ namespace Blish_HUD.Controls.Extern
     /// Struct representing a point.
     /// </summary>
     [StructLayout(LayoutKind.Sequential)]
-    public struct POINT
-    {
+    public struct POINT {
         public int X;
         public int Y;
 
-        public static implicit operator Point(POINT point)
-        {
+        public static implicit operator Point(POINT point) {
             return new Point(point.X, point.Y);
         }
     }
 
-    public enum CURSORFLAGS : int
-    {
-        CURSOR_HIDING = 0x0,
-        CURSOR_SHOWING = 0x1,
-        CURSOR_SUPPRESSED = 0x2
-    }
-
-    [StructLayout(LayoutKind.Sequential)]
-    public struct CURSORINFO
-    {
-        /// <summary>
-        /// The caller must set this to Marshal.SizeOf(typeof(CURSORINFO))
-        /// </summary>
-        public int cbSize;
-        public CURSORFLAGS flags;
-        public IntPtr hCursor;
-        public POINT ptScreenPos;
-    }
-
-    internal static class PInvoke
-    {
+    internal static class PInvoke {
         [DllImport("user32.dll")]
         internal static extern uint SendInput(uint nInputs, [MarshalAs(UnmanagedType.LPArray), In] Input[] pInputs, int cbSize);
 
@@ -79,16 +57,5 @@ namespace Blish_HUD.Controls.Extern
         [DllImport("user32.dll")]
         internal static extern bool GetCursorPos(out POINT lpPoint);
 
-        [DllImport("user32.dll")]
-        private static extern bool GetCursorInfo(ref CURSORINFO pci);
-
-        internal static CURSORINFO GetCursorInfo()
-        {
-            var pci = new CURSORINFO {
-                cbSize = Marshal.SizeOf(typeof(CURSORINFO))
-            };
-            GetCursorInfo(ref pci);
-            return pci;
-        }
     }
 }
---
+++ Blish HUD/GameServices/Input/WinApi/CursorExtern.cs
@@ -0,0 +1,18 @@
+﻿using System.Runtime.InteropServices;
+
+namespace Blish_HUD.Input.WinApi {
+    internal static class CursorExtern {
+
+        [DllImport("user32.dll")]
+        private static extern bool GetCursorInfo(ref CursorInfo pci);
+
+        internal static CursorInfo GetCursorInfo() {
+            var pci = new CursorInfo {
+                CbSize = Marshal.SizeOf(typeof(CursorInfo))
+            };
+            GetCursorInfo(ref pci);
+            return pci;
+        }
+
+    }
+}
