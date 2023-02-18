20e3a3658a6b200b061f256baa5f7cccf742077c
Dade Lamkins <dade@lamkins.org>
2023-02-18 13:13:57 -0500

    Fixed newline.

--- Blish HUD/Program.cs
+++ Blish HUD/Program.cs
@@ -49,7 +49,6 @@ namespace Blish_HUD {
         private static void Main(string[] args) {
             Directory.SetCurrentDirectory(Path.GetDirectoryName(Application.ExecutablePath));
 
-
             var settings = Cli.Parse<ApplicationSettings>(args);
 
             if (settings.MainProcessId.HasValue) {
