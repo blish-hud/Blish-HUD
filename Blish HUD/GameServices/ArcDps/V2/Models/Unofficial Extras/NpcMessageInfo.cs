db0c9eb1065643ae10c15df63796f88fca971e2e
Tim Nolte <22132579+Denrage@users.noreply.github.com>
2025-09-30 17:05:09 +0200

    Update Blish HUD/GameServices/ArcDps/V2/Models/Unofficial Extras/NpcMessageInfo.cs
    Co-authored-by: Flyga-M <155772554+Flyga-M@users.noreply.github.com>

--- Blish HUD/GameServices/ArcDps/V2/Models/Unofficial Extras/NpcMessageInfo.cs
+++ Blish HUD/GameServices/ArcDps/V2/Models/Unofficial Extras/NpcMessageInfo.cs
@@ -6,6 +6,10 @@ namespace Blish_HUD.GameServices.ArcDps.V2.Models.UnofficialExtras {
 
         public string Message { get; set; }
         
+        /// <summary>
+        /// Time since epoch in nanoseconds.
+        /// This can be used to sort messages, when they are out of order.
+        /// </summary>
         public ulong TimeStamp { get; set; }
     }
 }
