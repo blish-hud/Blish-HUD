7829e65e0dc63539dd7db70e5c0f89958f1c914f
Steven Liekens <steven.liekens@gmail.com>
2026-01-06 23:55:48 +0100

    Defer updating value until focus is lost

--- Blish HUD/Controls/FloatInput.cs
+++ Blish HUD/Controls/FloatInput.cs
@@ -11,8 +11,6 @@ using MouseEventArgs = Blish_HUD.Input.MouseEventArgs;
 namespace Blish_HUD.Controls {
 
     public class FloatInput : TextInputBase {
-        public event EventHandler<EventArgs>? EnterPressed;
-
         private const int TextPaddingX = 10;
 
         private const int SpinnerWidth = 32;
@@ -89,7 +87,7 @@ namespace Blish_HUD.Controls {
                 }
 
                 string text = value.ToString(_formatString, NumberFormatInfo.InvariantInfo);
-                if (Text != text) {
+                if (!string.Equals(Text, text, StringComparison.Ordinal)) {
                     Text = text;
                     Invalidate();
                     OnValueChanged();
@@ -301,12 +299,62 @@ namespace Blish_HUD.Controls {
         }
 
         protected override void HandleEnter() {
-            OnEnterPressed();
+            UnsetFocus();
         }
 
-        protected virtual void OnEnterPressed() {
-            UnsetFocus();
-            EnterPressed?.Invoke(this, EventArgs.Empty);
+        protected override void OnInputFocusChanged(ValueEventArgs<bool> e) {
+            base.OnInputFocusChanged(e);
+            if (!e.Value) {
+                ApplyTextAsValue();
+                _horizontalOffset = 0;
+                Invalidate();
+            }
+        }
+
+        private void OnValueChanged() {
+            ValueChanged?.Invoke(this, EventArgs.Empty);
+        }
+
+        private void OnGlobalMouseWheelScrolled(object sender, MouseEventArgs e) {
+            if (MouseOver) {
+                if (Input.Mouse.State.ScrollWheelValue > 0) {
+                    Value += _incrementAmount;
+                } else {
+                    Value -= _incrementAmount;
+                }
+            }
+        }
+
+        private void ApplyTextAsValue() {
+            if (string.IsNullOrEmpty(_text)) {
+                return;
+            }
+
+            StringBuilder numericBuilder = new StringBuilder(_text.Length);
+            ReadOnlySpan<char> input = _text.AsSpan();
+            bool hasDecimal = false;
+
+            foreach (char c in input) {
+                if (numericBuilder.Length == 0) {
+                    if (c == '+' || c == '-') {
+                        _ = numericBuilder.Append(c);
+                        continue;
+                    }
+                }
+
+                if (c >= '0' && c <= '9') {
+                    _ = numericBuilder.Append(c);
+                } else if (c == '.' && !hasDecimal) {
+                    _ = numericBuilder.Append(c);
+                    hasDecimal = true;
+                }
+            }
+
+            if (InvariantUtil.TryParseFloat(numericBuilder.ToString(), out float value)) {
+                Value = value;
+                Invalidate();
+                OnValueChanged();
+            }
         }
 
         /// <remarks>
@@ -409,60 +457,6 @@ namespace Blish_HUD.Controls {
             ValueChanged = null;
             Input.Mouse.MouseWheelScrolled -= OnGlobalMouseWheelScrolled;
             base.DisposeControl();
-        }
-
-        protected override void OnInputFocusChanged(ValueEventArgs<bool> e) {
-            base.OnInputFocusChanged(e);
-            if (!e.Value) {
-                Text = Value.ToString(_formatString, NumberFormatInfo.InvariantInfo);
-                _horizontalOffset = 0;
-                Invalidate();
-            }
-        }
-
-        protected override void OnTextChanged(ValueChangedEventArgs<string> e) {
-            base.OnTextChanged(e);
-            if (string.IsNullOrEmpty(_text)) {
-                return;
-            }
-
-            StringBuilder numericBuilder = new StringBuilder(_text.Length);
-            ReadOnlySpan<char> input = _text.AsSpan();
-            bool hasDecimal = false;
-
-            foreach (char c in input) {
-                if (numericBuilder.Length == 0) {
-                    if (c == '+' || c == '-') {
-                        _ = numericBuilder.Append(c);
-                        continue;
-                    }
-                }
-
-                if (c >= '0' && c <= '9') {
-                    _ = numericBuilder.Append(c);
-                } else if (c == '.' && !hasDecimal) {
-                    _ = numericBuilder.Append(c);
-                    hasDecimal = true;
-                }
-            }
-
-            if (InvariantUtil.TryParseFloat(numericBuilder.ToString(), out float value)) {
-                Value = value;
-            }
-        }
-
-        private void OnValueChanged() {
-            ValueChanged?.Invoke(this, EventArgs.Empty);
-        }
-
-        private void OnGlobalMouseWheelScrolled(object sender, MouseEventArgs e) {
-            if (MouseOver) {
-                if (Input.Mouse.State.ScrollWheelValue > 0) {
-                    Value += _incrementAmount;
-                } else {
-                    Value -= _incrementAmount;
-                }
-            }
         }
 
         private enum NumberInputAction {
